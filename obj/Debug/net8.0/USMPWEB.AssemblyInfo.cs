f0f3553b60b880aa35bf876558e002a56ed86628
Cetok <jtorvisco42@gmail.com>
2024-10-01 10:05:00 -0500

    fix

--- obj/Debug/net8.0/USMPWEB.AssemblyInfo.cs
+++ obj/Debug/net8.0/USMPWEB.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("USMPWEB")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c8fc70a4a326b837d30bb54a941255e42de16e82")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a4b9d3900d026ee54d9baa79326b5e288edc382b")]
 [assembly: System.Reflection.AssemblyProductAttribute("USMPWEB")]
 [assembly: System.Reflection.AssemblyTitleAttribute("USMPWEB")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
