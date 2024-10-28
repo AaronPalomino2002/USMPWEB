0e15c815226b1a51ba6afe86a91513334251a3eb
AaronPalomino2002 <palominofalconaaron@gmail.com>
2024-10-27 23:23:37 -0500

    Nuevos Detalles

--- Controllers/DetallesController.cs
+++ Controllers/DetallesController.cs
@@ -20,17 +20,53 @@ namespace USMPWEB.Controllers
             _apiBaseUrl = configuration["ApiSettings:BaseUrl"] ?? throw new InvalidOperationException("API base URL not found in configuration.");
         }
 
-        public async Task<IActionResult> Index(int id)
+        public async Task<IActionResult> Index(int id, string tipo)
         {
-            // Llamada a la API para obtener los detalles de la campaña
-            var campana = await _httpClient.GetFromJsonAsync<Campanas>($"{_apiBaseUrl}/Campanas/{id}");
-
-            if (campana == null)
+            if (string.IsNullOrEmpty(tipo))
             {
-                return NotFound();
+                return BadRequest("El tipo no puede estar vacío.");
             }
 
-            return View(campana);
+            if (tipo.Equals("campana", StringComparison.OrdinalIgnoreCase))
+            {
+                // Llamada a la API para obtener los detalles de la campaña
+                var campana = await _httpClient.GetFromJsonAsync<Campanas>($"{_apiBaseUrl}/Campanas/{id}");
+
+                if (campana == null)
+                {
+                    return NotFound();
+                }
+
+                return View("~/Views/Home/Detalles/Index.cshtml", campana); // Ruta completa a la vista de Campanas
+            }
+            else if (tipo.Equals("inscripcion", StringComparison.OrdinalIgnoreCase))
+            {
+                // Llamada a la API para obtener los detalles de la inscripción
+                var e_inscripcion = await _httpClient.GetFromJsonAsync<EventosInscripciones>($"{_apiBaseUrl}/EventosInscripciones/{id}");
+
+                if (e_inscripcion == null)
+                {
+                    return NotFound();
+                }
+
+                return View("~/Views/Inscripciones/Detalles/Index.cshtml", e_inscripcion); // Ruta completa a la vista de Inscripciones
+            }
+            else if (tipo.Equals("certificados", StringComparison.OrdinalIgnoreCase))
+            {
+                // Llamada a la API para obtener los detalles de la inscripción
+                var certificados = await _httpClient.GetFromJsonAsync<Certificados>($"{_apiBaseUrl}/Certificados/{id}");
+
+                if (certificados == null)
+                {
+                    return NotFound();
+                }
+
+                return View("~/Views/Certificados/Detalles/Index.cshtml", certificados); // Ruta completa a la vista de Inscripciones
+            }
+            else
+            {
+                return BadRequest("Tipo no válido. Debe ser 'campana', 'inscripcion', 'certficados' .");
+            }
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
