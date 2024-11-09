cfca6d01a252f114863ae17a3a1bf0efcd3b411a
Cetok <jtorvisco42@gmail.com>
2024-11-09 11:22:36 -0500

    PagoCertificados

---
+++ Models/CertificadoInscripcion.cs
@@ -0,0 +1,51 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using USMPWEB.Models;
+
+public class CertificadoInscripcion
+{
+    [Key]
+    public int Id { get; set; }
+    
+    [Required]
+    public long CertificadoId { get; set; }
+    
+    [Required]
+    [MaxLength(50)]
+    public string? NumeroRecibo { get; set; }
+    
+    [Required]
+    public string? Nombres { get; set; }
+    
+    [Required]
+    public string? Apellidos { get; set; }
+    
+    [Required]
+    public string? Matricula { get; set; }
+    
+    [Required]
+    public string? Facultad { get; set; }
+    
+    [Required]
+    public string? Carrera { get; set; }
+    
+    [Required]
+    public string? Direccion { get; set; }
+    
+    [Required]
+    public string? Telefono { get; set; }
+    
+    [Required]
+    [EmailAddress]
+    public string? Email { get; set; }
+    
+    [Required]
+    [Column(TypeName = "decimal(18,2)")]
+    public decimal Monto { get; set; }
+    
+    public DateTime FechaInscripcion { get; set; }
+    public string? Estado { get; set; }
+    public bool AceptoTerminos { get; set; }
+    [ForeignKey("CertificadoId")]  
+    public virtual Certificados? Certificado { get; set; }
+}
--- Models/ReciboViewModel.cs
+++ Models/ReciboViewModel.cs
@@ -7,7 +7,7 @@ namespace USMPWEB.Models
 {
     public class ReciboViewModel
     {
-        public int InscripcionId { get; set; } 
+        public int InscripcionId { get; set; }
         public string? NumeroRecibo { get; set; }
         public string? Nombres { get; set; }
         public string? Apellidos { get; set; }
@@ -20,15 +20,18 @@ namespace USMPWEB.Models
         public decimal Monto { get; set; }
         public DateTime FechaInscripcion { get; set; }
         public Campanas? Campana { get; set; }
+        public Certificados? Certificado { get; set; }
+        // Agregar propiedad para identificar el tipo
+        public string? TipoInscripcion { get; set; } // "Campana" o "Certificado"
         public string? Estado { get; set; }
         public string? QRCodeImage { get; set; }
         public void GenerarQR()
-    {
-        using var qrGenerator = new QRCodeGenerator();
-        var qrCodeData = qrGenerator.CreateQrCode(NumeroRecibo, QRCodeGenerator.ECCLevel.Q);
-        var qrCode = new PngByteQRCode(qrCodeData);
-        var qrCodeImage = qrCode.GetGraphic(20);
-        QRCodeImage = $"data:image/png;base64,{Convert.ToBase64String(qrCodeImage)}";
-    }
+        {
+            using var qrGenerator = new QRCodeGenerator();
+            var qrCodeData = qrGenerator.CreateQrCode(NumeroRecibo, QRCodeGenerator.ECCLevel.Q);
+            var qrCode = new PngByteQRCode(qrCodeData);
+            var qrCodeImage = qrCode.GetGraphic(20);
+            QRCodeImage = $"data:image/png;base64,{Convert.ToBase64String(qrCodeImage)}";
+        }
     }
 }
